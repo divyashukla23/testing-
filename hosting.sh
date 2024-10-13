6e55c42ce24a855da9282280080ee5cb2fe5bd6b
Divya Shukla <divyashukla20993@gmail.com>
2024-10-13 10:41:51 +0530

    task1 completed

--- hosting.sh
+++ hosting.sh
@@ -3,3 +3,5 @@ html_dir="/Users/divs/Desktop/Personal/GUVI/bash_scripting/web_hosting"
 cd "$html_dir" || { echo "Directory not found"; exit 1; }
 echo "hosting website at http://localhost:8080"
 http-server -p 8080
+//echo "hjdsvuygdydiugid"
+//dughiojddvfuyebe
